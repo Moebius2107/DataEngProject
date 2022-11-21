50a322fcdedcfef00b9fe2ed466d511689ac46a7
fcaste1901 <116746485+fcaste1901@users.noreply.github.com>
2022-11-21 14:29:27 +0100

    staging_dag_and_ingestion_changes

--- dags/ingestion_data_dag.py
+++ dags/ingestion_data_dag.py
@@ -1,11 +1,13 @@
 import airflow
 import datetime
 import urllib.request as request
-import pandas as pd
 import requests
 import random
 import json
 import glob
+from pymongo import MongoClient
+from pprint import pprint
+import warnings
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
@@ -80,6 +82,101 @@ download_projects_node = PythonOperator(
     depends_on_past=False,
 )
 
+download_projects_node = PythonOperator(
+    task_id='download_project_url_content',
+    dag=ingestion_data_dag,
+    trigger_rule='none_failed',
+    python_callable=_get_urls,
+    op_kwargs={
+        "dropbox_token": "",
+        "dropbox_link": "https://www.dropbox.com/sh/4i4tp6y0kl2lk24/AABMXKB4WetwcT_f1YoNtpbDa/projects?dl=0&lst=",
+        "destination_folder": "/opt/airflow/dags/data/raw_project_data",
+    },
+    depends_on_past=False,
+)
+
+def create_collections():
+    warnings.filterwarnings('ignore')
+
+    #we use the MongoClient to communicate with the running database instance.
+    myclient = MongoClient("mongodb://mongo:27017/") #Mongo URI format
+    mydb = myclient["customer_db"]
+
+    #Or you can use the attribute style 
+    #mydb = myclient.customer_db
+
+    print(mydb.list_collection_names())
+    participants = mydb["participants"]
+    hackatons = mydb["hackatons"]
+    projects = mydb["projects"]
+
+def ingest_collection(destination_collection, path, db):
+    import os
+    import json
+    # assign directory
+    directory = path
+    
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            with open(directory+'/'+filename) as file:
+                file_data = json.load(file)
+                x = destination_collection.insert_one(file_data)
+    hackaton=db.hackatons.find_one()
+    print(hackaton)
+
+create_collection = PythonOperator(
+    task_id='create_collections',
+    dag=ingestion_data_dag,
+    trigger_rule='none_failed',
+    python_callable=create_collections,
+    op_kwargs={},
+    depends_on_past=False,
+)
+
+
+
+ingest_mongo_hackaton = PythonOperator(
+    task_id='ingest_hackaton',
+    dag=ingestion_data_dag,
+    trigger_rule='none_failed',
+    python_callable=ingest_collection,
+    op_kwargs={
+        "destination_collection": "hackatons",
+        "path": "raw_hackaton_data",
+        "db": "mydb",
+    },
+    depends_on_past=False,
+)
+
+ingest_mongo_participant = PythonOperator(
+    task_id='ingest_participant',
+    dag=ingestion_data_dag,
+    trigger_rule='none_failed',
+    python_callable=ingest_collection,
+    op_kwargs={
+        "destination_collection": "participants",
+        "path": "raw_participant_data",
+        "db": "mydb",
+    },
+    depends_on_past=False,
+)
+
+ingest_mongo_project = PythonOperator(
+    task_id='ingest_project',
+    dag=ingestion_data_dag,
+    trigger_rule='none_failed',
+    python_callable=ingest_collection,
+    op_kwargs={
+        "destination_collection": "projects",
+        "path": "raw_project_data",
+        "db": "mydb",
+    },
+    depends_on_past=False,
+)
 
 
 eleventh_node = DummyOperator(
---
+++ dags/staging_data_dag.py
@@ -0,0 +1,85 @@
+import airflow
+import datetime
+import urllib.request as request
+import pandas as pd
+import requests
+import pymongo
+import json
+import glob
+from pprint import pprint
+import warnings
+from matplotlib import pyplot as plt
+import seaborn as sns
+from pymongo import MongoClient
+from sklearn.preprocessing import StandardScaler
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+
+
+default_args_dict = {
+    'start_date': datetime.datetime(2020, 6, 25, 0, 0, 0),
+    'concurrency': 1,
+    'schedule_interval': "0 0 * * *",
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+}
+
+staging_data_dag = DAG(
+    dag_id='staging_data_dag',
+    default_args=default_args_dict,
+    catchup=False,
+    template_searchpath=['/opt/airflow/dags/']
+)
+
+def get_mongodb_data(database_name, collection_name):
+    client = MongoClient()
+    db = client.database_name
+    collection = db.collection_name
+    data = pd.DataFrame(list(collection.find()))
+
+
+get_hackaton_data = PythonOperator(
+    task_id='get_hackaton_data',
+    dag=staging_data_dag,
+    trigger_rule='none_failed',
+    python_callable=get_mongodb_data,
+    op_kwargs={
+        "database_name": "mydb",
+        "collection_name": "hackatons",
+    },
+    depends_on_past=False,
+)
+
+get_participant_data = PythonOperator(
+    task_id='get_participant_data',
+    dag=staging_data_dag,
+    trigger_rule='none_failed',
+    python_callable=get_mongodb_data,
+    op_kwargs={
+        "database_name": "mydb",
+        "collection_name": "participants",
+    },
+    depends_on_past=False,
+)
+
+get_project_data = PythonOperator(
+    task_id='get_project_data',
+    dag=staging_data_dag,
+    trigger_rule='none_failed',
+    python_callable=get_mongodb_data,
+    op_kwargs={
+        "database_name": "mydb",
+        "collection_name": "projects",
+    },
+    depends_on_past=False,
+)
+
+eleventh_node = DummyOperator(
+    task_id='finale',
+    dag=staging_data_dag,
+    trigger_rule='none_failed'
+)
+
